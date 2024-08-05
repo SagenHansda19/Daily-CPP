7f2c1f2a1c195798d0e4f6acf3aefe366539281e
Sagen Hansda <sagenhansda19072003@gmail.com>
2024-08-05 19:21:09 +0530

    Provided

---
+++ A_very_big_sum.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+     int arr[10] = {};
+     int length;
+     cin>>length;
+     for (int i = 0; i<length; i++)
+     {
+          cin>>arr[i];
+     }
+     long sum = 0;
+     for (int i = 0; i<length ; i++)
+     {
+          sum+=arr[i];
+     }
+     return 0;
+}
