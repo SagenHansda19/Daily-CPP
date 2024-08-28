ed7caddada8bf666912b58965d113e93450cfa75
Sagen Hansda <sagenhansda19072003@gmail.com>
2024-08-28 21:47:07 +0530

    KLK

---
+++ _1512_Number_of_Good_Pairs.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
+class Solution {
+public:
+    int numIdenticalPairs(vector<int>& nums) {
+        int count = 0;
+        for (int i=0; i<nums.size(); i++){
+            for(int j=i; j<nums.size(); j++){
+                if((nums[i] == nums[j]) && i<j){
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+};
+
+int main(){
+     Solution sol;
+     vector<int> nums;
+     int val;
+     while(cin>>val){
+          nums.push_back(val);
+          if(cin.get() == '\n'){
+               break;
+          }
+     }
+     cout<<sol.numIdenticalPairs(nums);
+     return 0;
+}
