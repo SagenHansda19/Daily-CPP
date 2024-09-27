e3c1fd37cb81b1af90acb372e345748535047bd6
Sagen Hansda <sagenhansda19072003@gmail.com>
2024-09-27 18:34:39 +0530

    tm

---
+++ 110A_Nearly_Lucky_Number.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+#include <algorithm>
+#include <iostream>
+#include <vector>
+#include <string>
+#define ll long long
+using namespace std;
+int main(){
+     ll n;
+     cin>>n;
+     int count = 0;
+     bool flag = true;
+     while(n > 0){
+          int temp = n % 10;
+          n /= 10;
+          if(temp == 4 || temp == 7){
+               count++;
+          } 
+          else {
+               flag = false;
+                    break;
+          }
+     }
+     if(count == 4 || count == 7) cout<<"YES"<<endl;
+     else cout<<"NO"<<endl;
+     return 0;
+}
