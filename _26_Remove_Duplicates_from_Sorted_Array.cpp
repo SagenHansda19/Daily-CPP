a04957854a1028d97deaeb1a8ab837a325796f62
Sagen Hansda <sagenhansda19072003@gmail.com>
2024-10-23 20:27:39 +0530

    not a giver

---
+++ _26_Remove_Duplicates_from_Sorted_Array.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+#include <algorithm>
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
+class Solution {
+public:
+     int removeDuplicates(vector<int>& nums) {
+          int j = 1;
+          for(int i = 1; i < nums.size(); i++){
+               if(nums[i] != nums[i - 1]){
+                    nums[j] = nums[i];
+                    j++;
+               }
+          }
+          return j;
+     }
+};
+
+int main(){
+     Solution sol;
+     vector<int> nums;
+     int val;
+     while(cin>>val){
+          if(val == '\n') break;
+          nums.push_back(val);
+     }
+     cout<<sol.removeDuplicates(nums);
+     return 0;
+}
