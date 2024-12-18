e20525554c2125ba2d4c774d1a5b1e4ac8338c8a
Sagen Hansda <sagenhansda19072003@gmail.com>
2024-12-18 16:13:28 +0530

    deadend

---
+++ _2041E_Beautiful_Array.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {
+	int a, b;
+	cin >> a >> b;
+	cout << 3 << '\n';
+	cout << b << " " << b << " " << (3 * a - 2 * b) << '\n';
+}
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    solve();
+    return 0;
+}
+    
