04995f49474f518b1197a8d8b7772eeead2098ea
Sagen Hansda <sagenhansda19072003@gmail.com>
2025-03-12 22:46:27 +0530

    kalke abar start

---
+++ _1559A_Mocha_and_Math.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {
+    int n; cin >> n;
+    vector<int> a(n);
+    for(int &i : a) cin >> i;
+    int b = a.front();
+    for(int i : a) b &= i;
+    cout << b << '\n';
+}
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t; cin >> t;
+    while(t--) solve();
+    return 0;
+}
+
